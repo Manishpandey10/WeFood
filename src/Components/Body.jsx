269df6653d0bd0b6237320bccdefbcae17b47044
Manishpandey10 <manish2310pandey@gmail.com>
2025-03-17 12:23:51 +0530

    api fixes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -16,7 +16,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9698196&lng=77.7499721&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
     const apiData = await data.json();
     //
