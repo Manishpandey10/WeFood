7e3b72a7f47c8180e2e5e2f4c457b3faa19add0a
Manish Pandey <73751123+Manishpandey10@users.noreply.github.com>
2025-08-13 22:49:37 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,26 +3,26 @@ import { User } from "./User"
 import Type from "typed-js"
 import UserClass from "./UserClass"
 
-class About extends Component{
-    constructor(props){
-    super(props)     
-    
-    // console.log("this is parent constructor in about!");
-    
+class About extends Component {
+    constructor(props) {
+        super(props)
     }
-componentDidMount(){
-    // console.log("parent component mounted !!!!");
-}
-    render(){
-        // console.log("parent render function!");
-        return(        
-        <div>
-            <h1>About me </h1>
-            <h2> hey this is manish . i am engineering graduate.</h2>
-            <UserClass name = {"mxp"}contact={"@mxp23"}/>
 
-        </div>
+    componentDidMount() {
+        // console.log("parent component mounted !!!!");
+    }
+
+    render() {
+        return (
+            <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 text-center">
+                <h1 className="text-3xl font-bold mb-4">About Me</h1>
+                <h2 className="text-lg text-gray-700 mb-6">
+                    Hey, this is Manish. I am an engineering graduate.
+                </h2>
+                <UserClass name={"mxp"} contact={"@mxp23"} />
+            </div>
         )
     }
 }
-export default About;
+
+export default About
